59fdc132ec28dc19d47cb329323516f96de143ac
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-10-03 15:39:08 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"products",component:ProductsComponent},
+  {path:"",component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
